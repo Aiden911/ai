c31e73d4fbf652f854c5d5e37bd6a6f6a62d6088
APPY\Appy <forest19919111@gmail.com>
2024-11-10 00:15:53 +0800

    修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,    // 添加 node 環境
+    browser: true  // 添加瀏覽器環境
+  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
